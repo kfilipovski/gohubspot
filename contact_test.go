d72a367a80fcc6465410544500e63fd5ef250ace
Kire Filipovski <kire.filipovski@gmail.com>
2020-04-08 17:56:07 -0400

    Added tests for GetById and GetAll

--- contact_test.go
+++ contact_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func TestContactService_CreateContact(t *testing.T) {
+func TestContactService_Create(t *testing.T) {
 	setup()
 	defer teardown()
 
@@ -16,19 +16,37 @@ func TestContactService_CreateContact(t *testing.T) {
 	contact := Properties{}
 
 	contact.AddProperty("Email", "ion.suruceanu@gapsquare.com")
-	contact.AddProperty("NAME", "Ion Suruceanu")
 	contact.AddProperty("firstname", "Ion")
 	contact.AddProperty("lastname", "Suruceanu")
 	contact.AddProperty("lastlogindatetime", &now)
 
 	fmt.Println(contact)
 
-	ip, err := testclient.Contacts.Create(contact)
+	_, err := testclient.Contacts.Create(contact)
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	fmt.Println(ip)
+func TestContactService_GetById(t *testing.T) {
+	setup()
+	defer teardown()
 
-	t.Error("Just to test")
+	// demo id
+	id := 9332174
+
+	_, err := testclient.Contacts.GetById(id)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestContactService_GetAll(t *testing.T) {
+	setup()
+	defer teardown()
+
+	_, err := testclient.Contacts.GetAll()
+	if err != nil {
+		t.Error(err)
+	}
 }
