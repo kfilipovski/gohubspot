aa2f11666b20fa6f3731dfee2772058955d16598
Kire Filipovski <kire.filipovski@gmail.com>
2020-04-09 18:46:25 -0400

    Add company properties to GetAll

--- company.go
+++ company.go
@@ -15,7 +15,31 @@ type Companies struct {
 
 func (s *CompaniesService) GetAll() ([]*Company, error) {
 	url := "/companies/v2/companies/paged"
+
+	req, err := s.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	query := req.URL.Query()
+	query.Set("propertyMode", "value_only")
+	query.Set("property", "name")
+	query.Add("property", "domain")
+	query.Add("property", "website")
+	query.Add("property", "city")
+	query.Add("property", "state")
+	query.Add("property", "country")
+	query.Add("property", "industry")
+	query.Add("property", "lifecyclestage")
+	query.Add("property", "annualrevenue")
+	query.Add("property", "num_notes")
+	query.Add("property", "num_contacted_notes")
+	query.Add("property", "notes_last_updated")
+	query.Add("property", "notes_last_contacted")
+	req.URL.RawQuery = query.Encode()
+
 	all := new(Companies)
-	err := s.client.RunGet(url, all)
+	err = s.client.Do(req, all)
+
 	return all.Companies, err
 }
