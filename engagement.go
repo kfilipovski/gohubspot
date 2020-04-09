fc52219246227706341aeed552db70cfe522a062
Kire Filipovski <kire.filipovski@gmail.com>
2020-04-09 10:39:45 -0400

    Changed engagements GetAll to return engagement results with associations

--- engagement.go
+++ engagement.go
@@ -2,29 +2,34 @@ package gohubspot
 
 type EngagementsService service
 
-type EngagementType string
-
-const (
-	EngagementTypeNote    EngagementType = "NOTE"
-	EngagementTypeEmail   EngagementType = "EMAIL"
-	EngagementTypeTask    EngagementType = "TASK"
-	EngagementTypeMeeting EngagementType = "MEETING"
-	EngagementTypeCall    EngagementType = "CALL"
-)
-
 type Engagement struct {
-	Id         int                 `json:"id"`
-	PortalId   int                 `json:"portalId"`
-	DealId     int                 `json:"dealId"`
-	Active     bool                `json:"active"`
-	Properties map[string]Property `json:"properties"`
+	Id          int      `json:"id"`
+	PortalId    int      `json:"portalId"`
+	Active      bool     `json:"active"`
+	Type        string   `json:"type"`
+	BodyPreview string   `json:"bodyPreview"`
+	Source      string   `json:"source"`
+	SourceId    string   `json:"sourceId"`
+	CreatedAt   UnixTime `json:"createdAt"`
+	LastUpdated UnixTime `json:"lastUpdated"`
+	Timestamp   UnixTime `json:"timestamp"`
+}
+
+type Associations struct {
+	ContactIds []int `json:"contactIds"`
+}
+
+type EngagementResult struct {
+	Engagement   Engagement             `json:"engagement"`
+	Associations Associations           `json:"associations"`
+	Metadata     map[string]interface{} `json:"metadata"`
 }
 
 type Engagements struct {
-	Results []*Engagement `json:"results"`
+	Results []*EngagementResult `json:"results"`
 }
 
-func (s *EngagementsService) GetAll() ([]*Engagement, error) {
+func (s *EngagementsService) GetAll() ([]*EngagementResult, error) {
 	url := "/engagements/v1/engagements/paged"
 	all := new(Engagements)
 	err := s.client.RunGet(url, all)
